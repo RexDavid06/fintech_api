21f22fbc81b826becffa5754e206ac2ba68730eb
Buchi Rex-David <rhexmilia06@gmail.com>
2026-04-27 14:50:25 +0100

    fix: imported the authenticate method from django

--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK,  HTTP_201_CREATED, HTTP_401_UNAUTHORIZED,  HTTP_400_BAD_REQUEST
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
 
 User=get_user_model()
 
